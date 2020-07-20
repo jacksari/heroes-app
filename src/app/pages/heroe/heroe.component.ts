860261847bf6975ccb296bd13375ce14b219c796
Jack <janasarii@gmail.com>
2020-07-20 02:34:19 -0500

    first commit

---
+++ src/app/pages/heroe/heroe.component.ts
@@ -0,0 +1,88 @@
+import { Component, OnInit } from '@angular/core';
+import { HeroeModel } from 'src/app/models/heroe.model';
+import { NgForm } from '@angular/forms';
+import { HeroesService } from '../../services/heroes.service';
+import Swal from 'sweetalert2';
+import { Observable } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
+
+
+@Component({
+  selector: 'app-heroe',
+  templateUrl: './heroe.component.html',
+  styleUrls: ['./heroe.component.css']
+})
+export class HeroeComponent implements OnInit {
+
+  heroe = new HeroeModel();
+
+  constructor( private heroesService: HeroesService,
+               private route: ActivatedRoute ) { }
+
+  ngOnInit(): void {
+
+    const id = this.route.snapshot.paramMap.get('id');
+    // console.log(id);
+    if ( id !== 'nuevo' ) {
+      this.heroesService.gerHeroe( id ).subscribe( (resp: HeroeModel) => {
+        this.heroe = resp;
+        this.heroe.id = id;
+      } );
+    }
+
+  }
+
+  guardar( form: NgForm) {
+
+    if ( form.invalid ) {
+      console.log('Formulario no valido');
+      return;
+    }
+
+    Swal.fire({
+      icon: 'info',
+      title: 'Espere',
+      text: 'Guardando información',
+      allowOutsideClick: false,
+    });
+    Swal.showLoading();
+
+    let peticion: Observable<any>;
+
+
+    if ( this.heroe.id ) {
+      peticion = this.heroesService.actualizarHeroe( this.heroe );
+    } else {
+      peticion = this.heroesService.crearHeroe( this.heroe );
+    }
+
+    peticion.subscribe( resp => {
+
+      Swal.fire({
+        icon: 'success',
+        title: this.heroe.nombre,
+        text: 'Se actualizó correctamente',
+        
+      });
+      console.log(resp);
+      this.heroe.id = '';
+    this.heroe.nombre = '';
+    this.heroe.poder = '';
+    this.heroe.vivo = true;
+      
+
+    } );
+
+    
+
+
+
+
+    // console.log('fomr', form);
+    // console.log('heroe', this.heroe);
+
+
+
+  }
+
+}
---
+++ src/app/services/heroes.service.ts
@@ -0,0 +1,81 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { HeroeModel } from '../models/heroe.model';
+import { map, delay } from 'rxjs/operators';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroesService {
+
+  private url = 'https://fir-auth-8713e.firebaseio.com';
+
+  constructor( private http: HttpClient ){ }
+
+  crearHeroe(heroe: HeroeModel){
+
+    return this.http.post(`${ this.url }/heroes.json`, heroe)
+                  .pipe(
+                    map( (resp: any) => {
+                      heroe.id = resp.name;
+                      return heroe;
+                    } )
+                  );
+
+  }
+
+  actualizarHeroe( heroe: HeroeModel ){
+
+    const heroeTem = {
+      ...heroe
+    };
+
+    delete heroeTem.id;
+
+    return this.http.put(`${ this.url }/heroes/${ heroe.id }.json`, heroeTem);
+
+  }
+
+  getHeroes(){
+
+    return this.http.get(`${ this.url }/heroes.json`)
+                .pipe(
+                  map( resp => this.crearArreglo( resp )),
+                  delay(1000),
+                );
+
+  }
+
+  private crearArreglo( heroesObj: object ){
+
+    const heroes: HeroeModel[] = [];
+
+    //console.log(heroesObj);
+
+    if( heroesObj === null ){ return []; }
+
+    Object.keys( heroesObj ).forEach( key => {
+      const heroe: HeroeModel = heroesObj[key];
+      heroe.id = key;
+      heroes.push( heroe );
+    } );
+    
+    return heroes;
+
+  }
+
+  gerHeroe( id: string ){
+    return this.http.get(`${ this.url }/heroes/${ id }.json`);
+  }
+
+  borrarHeroe( id: string ){
+
+    return this.http.delete(`${ this.url }/heroes/${ id }.json`);
+
+  }
+
+
+
+
+}
